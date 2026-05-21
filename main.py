ae4891e59379cc7520d22e0a55475e985f9367ad
Osaid Hassan <hassanosaid123@gmail.com>
2026-05-21 11:15:54 +0000

    api working, admin commands not workin yet

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import json
-#from fastapi import FastAPI
-#import uvicorn
+from fastapi import FastAPI, HTTPException
+import uvicorn
 from random import randint
 
-#app = FastAPI()
+adminkey = "onlyiaddorremove"
+
+app = FastAPI()
 FILE = "quotes.json"
 
+@app.get("/get")
 def get():
     temp_list: list = []
     with open (FILE) as f:
@@ -13,45 +16,40 @@ def get():
     num_of_quotes = len(temp_list)
     random_num = randint(0, num_of_quotes - 1)
     quote = temp_list[random_num]
-    print(quote) # changes this
+    return{"quote": quote}
 
-def add():
-    num_of_quotes = input("How many quotes will you add: ") # changes
-    num_of_quotes = int(num_of_quotes)
+@app.post("/add/{quote}/{admin_key}")
+def add(quote: str, admin_key: str):
+    if admin_key != adminkey:
+        raise HTTPException(status_code=403, detail="Invalid admin key")
+        return{"error": "Invalid admin key"}
     temp_list: list = []
     with open (FILE, "r") as f:
         temp_list = json.load(f)
-    for i in range(num_of_quotes):
-        quote: str = ""
-        quote = input("Enter your quote: ") # changes
-        temp_list.append(quote)
+    temp_list.append(quote)
     with open (FILE, "w") as f:
         json.dump(temp_list, f)
+    return{"confirmation": "quotes added successfully"}
 
-def remove():
-    num_of_quotes = input("How many quotes will you remove: ") # changes
-    num_of_quotes = int(num_of_quotes)
+@app.post("/remove")
+def remove(quote: int, admin_key: str):
+    if admin_key != adminkey:
+        raise HTTPException(status_code=403, detail="Invalid admin key")
+        return{"error": "Invalid admin key"}
     temp_list: list = []
     with open (FILE) as f:
         temp_list = json.load(f)
-        print(temp_list)
-    removing_list: list = []
-    for i in range(num_of_quotes):
-        quote_num = input("Enter the quote index: ") # changes
-        quote_num = int(quote_num)
-        removing_list.append(quote_num)
-    removing_list.sort(reverse = True)
-    for i in range(len(removing_list)):
-        if 0 <= removing_list[i] < len(temp_list):
-            temp_list.pop(removing_list[i])
+    if 0 <= quote < len(temp_list):
+        temp_list.pop(quote)
     with open (FILE, "w") as f:
         json.dump(temp_list, f)
+    return{"confirmation": "quotes removed successfully"}
     
-
+@app.get("/list")
 def list():
     temp_list: list = []
     with open (FILE, "r") as f:
         temp_list = json.load(f)
-    print(temp_list) # changes this
+    return{"quotes": temp_list}
 
 
